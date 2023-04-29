a21f6305d4eefd1ce3e1680035db03ff4dec109d
Nover-WD <89575046+Nover-WD@users.noreply.github.com>
2023-04-29 21:50:26 +0800

    Update Shop.js

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -4,12 +4,25 @@ import Trending from "../layout/Trending";
 import PrimaryMenu from "../PrimaryMenu";
 import Footer from "../Footer";
 
+import { styled, makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+    root: {
+        '@media (max-width: 599px)': {
+            padding: '30px 30px',
+            marginTop: '92px'
+        }
+    }
+})
+
 export default function Shop(){
+    const classes = useStyles();
+    
     return(
         <>
         <TopBanner/>
         <PrimaryMenu/>
-        <Trending title="Shop"/>
+        <Trending title="Shop" className={classes.root}/>
         <Footer/>
         </>
     )
